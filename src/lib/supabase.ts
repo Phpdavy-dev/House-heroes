0733d09d3774576467b135ce34561e71c3d808c7
Phpdavy-dev <68374205+Phpdavy-dev@users.noreply.github.com>
2026-06-10 13:27:18 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 // Vul hieronder je eigen Supabase-gegevens in:
-const SUPABASE_URL = "https://evrxuczknavtnyivtzfi.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://evrxuczknavtnyivtzfi.supabase.co/";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2cnh1Y3prbmF2dG55aXZ0emZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwNDc3NjgsImV4cCI6MjA5NjYyMzc2OH0.D8G1cQGvmrD_UuQJGfSEcouVHrJV78lkU_MM9BFbb5I";
 
 let client: SupabaseClient | null = null;
